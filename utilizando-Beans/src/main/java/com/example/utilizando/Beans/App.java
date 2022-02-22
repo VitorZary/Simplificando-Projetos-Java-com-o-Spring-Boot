507aac90396a8bd4219ebadb6337fc802b324681
Vitor Hugo Kuhn Zary <vitorhbts@gmail.com>
2022-02-22 15:38:34 -0300

    Implementando um codigo usando beans

---
+++ utilizando-Beans/src/main/java/com/example/utilizando/Beans/App.java
@@ -0,0 +1,21 @@
+package com.example.utilizando.Beans;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        ApplicationContext factory = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Livro livro = factory.getBean(Livro.class);
+        livro.setNome("Harry Potter");
+        livro.setCodigo("DFAS23");
+
+        Autor autor = factory.getBean(Autor.class);
+        autor.setNome("J.K. Rowling");
+
+        livro.exibir();
+    }
+}
---
+++ utilizando-Beans/src/main/java/com/example/utilizando/Beans/AutorLivro.java
@@ -0,0 +1,7 @@
+package com.example.utilizando.Beans;
+
+public interface AutorLivro {
+
+    void exibirAutor();
+
+}
