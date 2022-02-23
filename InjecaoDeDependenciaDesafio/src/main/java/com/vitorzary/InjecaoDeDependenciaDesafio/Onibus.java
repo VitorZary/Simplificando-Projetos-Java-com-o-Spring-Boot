a5d57cb6d4cb118b72262ee8968ce9c718e97444
Vitor Hugo Kuhn Zary <vitorhbts@gmail.com>
2022-02-23 18:57:56 -0300

    Concluindo desafio

---
+++ InjecaoDeDependenciaDesafio/src/main/java/com/vitorzary/InjecaoDeDependenciaDesafio/Onibus.java
@@ -0,0 +1,11 @@
+package com.vitorzary.InjecaoDeDependenciaDesafio;
+
+public class Onibus implements Veiculo{
+
+    @Override
+    public void acao(){
+        System.out.println("É um ônibus");
+    }
+
+
+}
---
+++ InjecaoDeDependenciaDesafio/src/test/java/com/vitorzary/InjecaoDeDependenciaDesafio/InjecaoDeDependenciaDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorzary.InjecaoDeDependenciaDesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InjecaoDeDependenciaDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
