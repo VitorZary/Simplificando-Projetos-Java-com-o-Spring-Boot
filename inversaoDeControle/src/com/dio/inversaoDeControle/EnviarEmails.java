c23e3e3b980beb7251756922082dbcb1b5da3223
Vitor Hugo Kuhn Zary <vitorhbts@gmail.com>
2022-02-23 09:36:28 -0300

    Codigo aula inversao de controle

---
+++ inversaoDeControle/src/com/dio/inversaoDeControle/EnviarEmails.java
@@ -0,0 +1,19 @@
+package com.dio.inversaoDeControle;
+
+public class EnviarEmails {
+
+    public EnviarEmails(String tipo, String endereco, String senha){
+
+    }
+
+    public static EnviarEmails obterDadosEmail(){
+        return new EnviarEmails("smtp", "contato@email.com", "123senha");
+    }
+
+    public void retornar(String mensagem){
+        System.out.println("Email enviado!");
+    }
+
+
+
+}
